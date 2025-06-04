ec06339197e184a2acdee12df01b4b12dbc757f1
Santiagomarji <marjighaith26@gmail.com>
2025-06-03 19:17:14 -0700

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello from MondoAI!</h1>
+      <p>Your app is working 🎉</p>
+    </div>
+  )
+}
+
+export default App
